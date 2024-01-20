f9d7ae1ab67941490eec1029c0a661f4bc6997f4
camilakin-ms <149860053+camilakin-ms@users.noreply.github.com>
2024-01-20 18:36:22 -0500

    Update background.js
    from cami's branch

--- StudyNight/background.js
+++ StudyNight/background.js
@@ -1,49 +1,44 @@
-const ALARM_JOB_NAME = "CHECK_ALARM"
-chrome.runtime.onMessage.addListener(data=> {
-    let {event, task} = data
-    if(event == 'taskSubmitted'){
-        handleTaskSubmit(task);
+chrome.runtime.onMessage.addListener(data=> { //listens to submit tasks button
+    let {event, tasks} = data
+    if(event == 'tasksSubmitted'){
+        handleTaskSubmit(tasks);
     }
 })
 
-// when the alarm is on. dictated by the timer it will check
-
-let handleTaskSubmit = (task) => {
-    console.log("Task received in background.");
-    console.log("Task received: ", task);
-    chrome.storage.local.set(task)
-    createAlarm();
+let handleTaskSubmit = (tasks) => { //logs tasks input upon button pressed
+    console.log("Tasks received in background.");
+    console.log("Tasks received: ", tasks);
 }
 
-const createAlarm = () => {
-	chrome.alarms.create(ALARM_JOB_NAME, {periodInMinutes: 1.0 })
-}
-const stopAlarm = () => {
-	chrome.alarms.clearAll()
-}
-chrome.alarms.onAlarm.addListener(() => {
-	console.log("onAlarm schedule code running...")
-	let queryOptions = {active: true, lastFocusedWindow: true};
-	
-	chrome.tabs.query(queryOptions, (tabs) => {
-		if (chrome.runtime.lastError) {
-			console.error(chrome.runtime.lastError);
-			return;
-		}
-		
-		if(tabs && tabs.length > 0) {
-			const activeTab = tabs[0];
-        		const tabTitle = activeTab.title;
-        		const tabUrl = activeTab.url;
-        		
-        	
-        	console.log("Active Tab Title: " + tabTitle);
-        	console.log("Active Tab URL: " + tabUrl);
-        	
-        	}else {
-        		console.error(" no tab found");
-        		}
-        	
-	
-	})
+chrome.runtime.onMessage.addListener(data => { //listens to checkboxes
+    if(data.event === 'checked1'){
+        console.log("checklist 1 IS CHECKED")
+    }
+    if(data.event === 'unchecked1'){
+        console.log("checklist 1 IS UNCHECKED")
+    }
+    if(data.event === 'checked2'){
+        console.log("checklist 2 IS CHECKED")
+    }
+    if(data.event === 'unchecked2'){
+        console.log("checklist 2 IS UNCHECKED")
+    }
+    if(data.event === 'checked3'){
+        console.log("checklist 3 IS CHECKED")
+    }
+    if(data.event === 'unchecked3'){
+        console.log("checklist 3 IS UNCHECKED")
+    }
+    if(data.event === 'checked4'){
+        console.log("checklist 4 IS CHECKED")
+    }
+    if(data.event === 'unchecked4'){
+        console.log("checklist 4 IS UNCHECKED")
+    }
+    if(data.event === 'checked5'){
+        console.log("checklist 5 IS CHECKED")
+    }
+    if(data.event === 'unchecked5'){
+        console.log("checklist 5 IS UNCHECKED")
+    }
 })
